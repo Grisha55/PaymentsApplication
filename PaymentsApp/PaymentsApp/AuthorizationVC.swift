4eda1b387b98ded7dca52ad2a4613bd0b18c8115
Grigory <grishavinyar@icloud.com>
2021-07-02 11:52:14 +0300

    Создал проект

---
+++ PaymentsApp/PaymentsApp/AuthorizationVC.swift
@@ -0,0 +1,18 @@
+//
+//  AuthorizationVC.swift
+//  PaymentsApp
+//
+//  Created by Григорий Виняр on 02.07.2021.
+//
+
+import UIKit
+
+class AuthorizationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+
+}
