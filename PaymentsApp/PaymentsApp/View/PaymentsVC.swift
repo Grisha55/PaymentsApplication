73ced6f962a6948a72d2e7d5ea3c64ca2bbeb72f
Grigory <grishavinyar@icloud.com>
2021-07-03 09:38:59 +0300

    Чиста проекта

--- PaymentsApp/PaymentsApp/View/PaymentsVC.swift
+++ PaymentsApp/PaymentsApp/View/PaymentsVC.swift
@@ -67,14 +67,6 @@ class PaymentsVC: UIViewController {
         tableView.register(PaymentCell.self, forCellReuseIdentifier: PaymentCell.paymentCell)
         tableView.showsVerticalScrollIndicator = false
     }
-    
-//    private func setTableViewConstraints() {
-//        tableView.translatesAutoresizingMaskIntoConstraints                        = false
-//        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive           = true
-//        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive     = true
-//        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive   = true
-//    }
 
 }
 
